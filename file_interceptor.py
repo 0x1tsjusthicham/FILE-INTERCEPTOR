42da8827c2ac51265558001cb70cb64aab74baa4
0x1tsjusthicham <hicham.ezzamzami@gmail.com>
2024-10-01 13:31:00 -0400

    Filter for exe request

--- file_interceptor.py
+++ file_interceptor.py
@@ -5,8 +5,9 @@ def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.Raw):
         if scapy_packet[scapy.TCP].dport == 80:
-            print("HTTP Request")
-            print(scapy_packet.show())
+            if ".exe" in str(scapy_packet[scapy.Raw].load):
+                print("EXE Request")
+                print(scapy_packet.show())
         elif scapy_packet[scapy.TCP].sport == 80:
             print("HTTP Response")
             print(scapy_packet.show())
